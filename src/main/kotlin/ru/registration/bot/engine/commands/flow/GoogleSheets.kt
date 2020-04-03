0fce229c5a85a74057499c23073d48fba60e2318
d.bychkov <d.bychkov@tinkoff.ru>
2020-04-03 10:05:38 +0300

    #1 fix

--- src/main/kotlin/ru/registration/bot/engine/commands/flow/GoogleSheets.kt
+++ src/main/kotlin/ru/registration/bot/engine/commands/flow/GoogleSheets.kt
@@ -1,7 +1,5 @@
 package ru.registration.bot.engine.commands.flow
 
-import com.google.api.client.json.JsonFactory
-import com.google.api.client.json.jackson2.JacksonFactory
 import org.telegram.telegrambots.meta.api.objects.User
 import ru.registration.bot.engine.CommonFactory
 import ru.registration.bot.repositories.specifications.UserRequest
@@ -11,10 +9,6 @@ class GoogleSheets(
     private val user: User?
 )
 {
-    companion object {
-        val jacksonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
-    }
-
     fun send() {
         val request = commonFactory.requestRepository.query(UserRequest(user?.id)).first()
         commonFactory.googleSheets.send(request)
--- src/main/kotlin/ru/registration/bot/google/GoogleSheetsService.kt
+++ src/main/kotlin/ru/registration/bot/google/GoogleSheetsService.kt
@@ -1,6 +1,8 @@
 package ru.registration.bot.google
 
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.JsonFactory
+import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.sheets.v4.Sheets
 import com.google.api.services.sheets.v4.model.ValueRange
 import com.google.auth.Credentials
@@ -18,6 +20,11 @@ class GoogleSheetsService(
     @Value("\${services.sheets.range}") private val range: String
 ) {
 
+    companion object {
+        // todo вынести в конфигурацию
+        val jacksonFactory: JsonFactory = JacksonFactory.getDefaultInstance()
+    }
+
     private lateinit var service: Sheets
 
     @PostConstruct
@@ -25,7 +32,7 @@ class GoogleSheetsService(
         val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
 
         service = Sheets.Builder(httpTransport,
-            GoogleSheets.jacksonFactory, HttpCredentialsAdapter(obtainCredentials())
+            jacksonFactory, HttpCredentialsAdapter(obtainCredentials())
         )
             .setApplicationName("registration-bot")
             .build()
