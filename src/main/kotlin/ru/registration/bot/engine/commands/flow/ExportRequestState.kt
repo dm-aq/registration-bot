20e84bd42710721f269241559da55f94d21fc144
d.bychkov <d.bychkov@tinkoff.ru>
2020-04-10 17:48:22 +0300

    final message fixed

--- src/main/kotlin/ru/registration/bot/engine/commands/flow/ExportRequestState.kt
+++ src/main/kotlin/ru/registration/bot/engine/commands/flow/ExportRequestState.kt
@@ -19,7 +19,10 @@ class ExportRequestState(
     fun export() {
         commonFactory.stateRepo.execute(SetUserStatus(user?.id, StateType.NEIGHBORS_STATE, StateType.REQUEST_READY))
 
-        absSender?.execute(SendMessage(chat?.id, "Спасибо! Заявка заполнена."))
+        absSender?.execute(SendMessage(chat?.id, """
+            Спасибо! Заявка заполнена.
+            Мы свяжемся с вами в течении 24 часов.
+        """.trimIndent()))
 
         val request = commonFactory.requestRepository.query(UserRequest(user?.id, StateType.REQUEST_READY)).first()
 
