68065d846236e9c942b21d4ed07114cd6f53eb0d
d.bychkov <dmitry.bychkov@tinkoff.ru>
2021-08-15 18:29:32 +0300

    StartState test

--- src/test/kotlin/ru/registration/bot/engine/commands/flow/StartStateTest.kt
+++ src/test/kotlin/ru/registration/bot/engine/commands/flow/StartStateTest.kt
@@ -5,11 +5,13 @@ import com.nhaarman.mockitokotlin2.argumentCaptor
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.Answers
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Chat
+import org.telegram.telegrambots.meta.api.objects.Update
 import org.telegram.telegrambots.meta.api.objects.User
 import org.telegram.telegrambots.meta.bots.AbsSender
 import ru.registration.bot.engine.CommonFactory
@@ -29,9 +31,13 @@ class StartStateTest {
         val absSender: AbsSender = mock()
         val commonFactory: CommonFactory = mock(defaultAnswer = Answers.RETURNS_DEEP_STUBS)
         val startState = StartState(chat, user, absSender, commonFactory)
+        val update: Update = mock(defaultAnswer = Answers.RETURNS_DEEP_STUBS) {
+            on { hasCallbackQuery() } doReturn false
+            on { message?.text } doReturn "new_registration"
+        }
 
         // act
-        startState.ask()
+        startState.handle(update)
 
         // assert
         verify(commonFactory.requestRepository).execute(any())
@@ -47,7 +53,27 @@ class StartStateTest {
         verify(absSender).execute(messageCaptor.capture())
         assertEquals(1, messageCaptor.firstValue.chatId.toInt())
         assertEquals("Введите ваш номер телефона:", messageCaptor.firstValue.text)
+    }
 
+    @Test
+    fun `not new registration`() {
+        // arrange
+        val update: Update = mock(defaultAnswer = Answers.RETURNS_DEEP_STUBS) {
+            on { hasCallbackQuery() } doReturn false
+            on { message?.text } doReturn "another command"
+        }
+        val user: User = mock()
+        val chat: Chat = mock()
+        val absSender: AbsSender = mock()
+        val commonFactory: CommonFactory = mock(defaultAnswer = Answers.RETURNS_DEEP_STUBS)
+        val startState = StartState(chat, user, absSender, commonFactory)
 
+        // act
+        startState.handle(update)
+
+        // assert
+        verifyZeroInteractions(commonFactory.requestRepository)
+        verifyZeroInteractions(commonFactory.stateRepo)
+        verifyZeroInteractions(absSender)
     }
 }
