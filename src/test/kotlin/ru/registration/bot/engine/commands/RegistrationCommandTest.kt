9fac25869ef0c77b5a69187392c877164b583aa5
d.bychkov <dmitry.bychkov@tinkoff.ru>
2021-09-11 15:12:22 +0300

    registration command test

--- src/test/kotlin/ru/registration/bot/engine/commands/RegistrationCommandTest.kt
+++ src/test/kotlin/ru/registration/bot/engine/commands/RegistrationCommandTest.kt
@@ -1,11 +1,58 @@
 package ru.registration.bot.engine.commands
 
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.given
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Answers.RETURNS_DEEP_STUBS
+import org.telegram.telegrambots.meta.api.objects.Chat
+import org.telegram.telegrambots.meta.api.objects.Message
+import org.telegram.telegrambots.meta.api.objects.User
+import org.telegram.telegrambots.meta.bots.AbsSender
+import ru.registration.bot.engine.CommonFactory
+import ru.registration.bot.engine.commands.flow.State
 
 class RegistrationCommandTest {
+
+    lateinit var commonFactory: CommonFactory
+    lateinit var registrationCommand: RegistrationCommand
+
+    @BeforeEach
+    fun setUp() {
+        commonFactory = mock()
+        registrationCommand = RegistrationCommand(false, commonFactory)
+    }
+
     @Test
     fun `new customer coming`() {
-        TODO("Not yet implemented")
+        // arrange
+        val absSender: AbsSender = mock(defaultAnswer = RETURNS_DEEP_STUBS)
+
+        val userId = 123
+        val user: User = mock{
+            on { id } doReturn 123
+        }
+        val chatId = 1L
+        val chat: Chat = mock{
+            on { id } doReturn chatId
+        }
+        val message: Message = mock {
+            on { this.from } doReturn user
+            on { this.chat } doReturn chat
+        }
+        val startState: State = mock()
+        given { commonFactory.create(any(), any(), any(), anyOrNull()) }.willReturn(startState)
+
+        // act
+        registrationCommand.processMessage(absSender, message, null)
+
+        // assert
+        verify(startState).ask(eq(userId), eq(chatId), any())
     }
 
     @Test
