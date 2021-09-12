1843feb5a8e72e0003b8d98ad79a7d631616c9b3
d.bychkov <dmitry.bychkov@tinkoff.ru>
2021-09-12 20:58:26 +0300

    registration command tests

--- src/test/kotlin/ru/registration/bot/engine/commands/RegistrationCommandTest.kt
+++ src/test/kotlin/ru/registration/bot/engine/commands/RegistrationCommandTest.kt
@@ -2,40 +2,40 @@ package ru.registration.bot.engine.commands
 
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.argumentCaptor
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.given
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
-import org.junit.jupiter.api.BeforeEach
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.Answers.RETURNS_DEEP_STUBS
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Chat
 import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.User
 import org.telegram.telegrambots.meta.bots.AbsSender
 import ru.registration.bot.engine.CommonFactory
 import ru.registration.bot.engine.commands.flow.State
+import ru.registration.bot.engine.commands.flow.states.SexState
+import ru.registration.bot.repositories.RequestRepository
+import ru.registration.bot.repositories.StateRepository
 
 class RegistrationCommandTest {
-
-    lateinit var commonFactory: CommonFactory
-    lateinit var registrationCommand: RegistrationCommand
-
-    @BeforeEach
-    fun setUp() {
-        commonFactory = mock()
-        registrationCommand = RegistrationCommand(false, commonFactory)
-    }
 
     @Test
     fun `new customer coming`() {
         // arrange
+        val commonFactory: CommonFactory = mock()
+        val registrationCommand = RegistrationCommand(false, commonFactory)
         val absSender: AbsSender = mock(defaultAnswer = RETURNS_DEEP_STUBS)
 
         val userId = 123
         val user: User = mock{
-            on { id } doReturn 123
+            on { id } doReturn userId
         }
         val chatId = 1L
         val chat: Chat = mock{
@@ -57,26 +57,123 @@ class RegistrationCommandTest {
 
     @Test
     fun `customer has draft`() {
-        TODO("Not yet implemented")
-    }
+        // arrange
+        val commonFactory: CommonFactory = mock()
+        val registrationCommand = RegistrationCommand(false, commonFactory)
+        val absSender: AbsSender = mock(defaultAnswer = RETURNS_DEEP_STUBS)
 
-    @Test
-    fun `customer has finisher request`() {
-        TODO("Not yet implemented")
+        val userId = 123
+        val user: User = mock{
+            on { id } doReturn userId
+        }
+        val chatId = 1L
+        val chat: Chat = mock{
+            on { id } doReturn chatId
+        }
+        val message: Message = mock {
+            on { this.from } doReturn user
+            on { this.chat } doReturn chat
+        }
+
+        val stateRepo: StateRepository = mock()
+        val requestRepo: RequestRepository = mock()
+        val nextState: State = mock()
+        val state = SexState(stateRepo, requestRepo, nextState)
+        given { commonFactory.create(any(), any(), any(), anyOrNull()) }.willReturn(state)
+
+        // act
+        registrationCommand.processMessage(absSender, message, null)
+
+        // assert
+        val messageCaptor = argumentCaptor<SendMessage>()
+        verify(absSender, times(2)).execute(messageCaptor.capture())
+        assertEquals(chatId, messageCaptor.firstValue.chatId.toLong())
+        assertEquals("Давайте заполним оставшиеся поля в черновике.", messageCaptor.firstValue.text)
     }
 
     @Test
     fun `registration is closed`() {
-        TODO("Not yet implemented")
+        // arrange
+        val commonFactory: CommonFactory = mock()
+        val registrationCommand = RegistrationCommand(true, commonFactory)
+        val absSender: AbsSender = mock(defaultAnswer = RETURNS_DEEP_STUBS)
+
+        val user: User = mock{
+            on { id } doReturn 123
+        }
+        val chatId = 1L
+        val chat: Chat = mock{
+            on { id } doReturn chatId
+        }
+        val message: Message = mock {
+            on { this.from } doReturn user
+            on { this.chat } doReturn chat
+        }
+        val startState: State = mock()
+        given { commonFactory.create(any(), any(), any(), anyOrNull()) }.willReturn(startState)
+
+        // act
+        registrationCommand.processMessage(absSender, message, null)
+
+        // assert
+        val messageCaptor = argumentCaptor<SendMessage>()
+        verify(absSender).execute(messageCaptor.capture())
+        assertEquals(chatId, messageCaptor.firstValue.chatId.toLong())
+        assertEquals("Регистрация на выезд закрыта", messageCaptor.firstValue.text)
+
+        verifyZeroInteractions(startState)
     }
 
     @Test
     fun `absSender is absent`() {
-        TODO("Not yet implemented")
+        // arrange
+        val commonFactory: CommonFactory = mock()
+        val registrationCommand = RegistrationCommand(false, commonFactory)
+        val absSender: AbsSender? = null
+
+        val user: User = mock{
+            on { id } doReturn 123
+        }
+        val chatId = 1L
+        val chat: Chat = mock{
+            on { id } doReturn chatId
+        }
+        val message: Message = mock {
+            on { this.from } doReturn user
+            on { this.chat } doReturn chat
+        }
+        val startState: State = mock()
+        given { commonFactory.create(any(), any(), any(), anyOrNull()) }.willReturn(startState)
+
+        // act
+        registrationCommand.processMessage(absSender, message, null)
+
+        // assert
+        verifyZeroInteractions(startState)
     }
 
     @Test
     fun `message is absent`() {
-        TODO("Not yet implemented")
+        // arrange
+        val commonFactory: CommonFactory = mock()
+        val registrationCommand = RegistrationCommand(false, commonFactory)
+        val absSender: AbsSender = mock(defaultAnswer = RETURNS_DEEP_STUBS)
+
+        val user: User = mock{
+            on { id } doReturn 123
+        }
+        val chatId = 1L
+        val chat: Chat = mock{
+            on { id } doReturn chatId
+        }
+        val message: Message? = null
+        val startState: State = mock()
+        given { commonFactory.create(any(), any(), any(), anyOrNull()) }.willReturn(startState)
+
+        // act
+        registrationCommand.processMessage(absSender, message, null)
+
+        // assert
+        verifyZeroInteractions(startState)
     }
 }
